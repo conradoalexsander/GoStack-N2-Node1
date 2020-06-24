360752de89e73afca44b709185664b5cfa76976d
Conrado Alexsander <conradoalexsander@hotmail.com>
2020-06-23 21:24:42 -0300

    Added Get route

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,26 +1,32 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    console.log('cheguei aqui');
+    const transactions = [
+      transactionsRepository.all(),
+      transactionsRepository.getBalance(),
+    ];
+
+    return response.json(transactions);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
 });
 
-transactionRouter.post('/', (request, response) => {
-  try {
-    // TODO
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
-});
+// transactionRouter.post('/', (request, response) => {
+//   try {
+//     // TODO
+//   } catch (err) {
+//     return response.status(400).json({ error: err.message });
+//   }
+// });
 
 export default transactionRouter;
